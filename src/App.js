86572ebfa05bf54d141748329c5bc8d994acf333
Giancarlo Nigrinis Cantillo <gnigrinis@gmail.com>
2022-04-13 17:34:10 -0500

    Adding NavBar with Tailwind and DaisyUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tienda Geek</h1>
+      <NavBar/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
